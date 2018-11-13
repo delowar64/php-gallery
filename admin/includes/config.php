3188b984a797115d4e4378ec4f5dcebf8799fae8
Delowar Hossain <delowar@DMP.local>
2018-11-13 23:32:18 +0600

    dashboard setup

---
+++ admin/includes/config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: delowar
+ * Date: 11/13/18
+ * Time: 11:30 PM
+ */
---
+++ admin/index.php
@@ -0,0 +1,36 @@
+<?php
+include("includes/header.php");
+include ("includes/navigation.php");
+?>
+
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Dashboard
+                        <small>Subheading</small>
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li>
+                            <i class="fa fa-dashboard"></i>  <a href="index.php">Admin</a>
+                        </li>
+                        <li class="active">
+                            <i class="fa fa-file"></i> Dashboard
+                        </li>
+                    </ol>
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+<?php include("includes/footer.php"); ?>
